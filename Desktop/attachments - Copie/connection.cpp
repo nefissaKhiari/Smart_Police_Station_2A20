b574cceb39b7414cbb97279d9ce704d5104cbc3a
ahmed407696 <ahmed.ayedi@esprit.tn>
2021-03-16 12:09:11 +0100

    first  commit

--- Desktop/attachments - Copie/connection.cpp
+++ Desktop/attachments - Copie/connection.cpp
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("projet_2a20");//inserer le nom de la source de données ODBC
-db.setUserName("ahmed");//inserer nom de l'utilisateur
-db.setPassword("ahmed");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("Projet_2A20");//inserer le nom de la source de données ODBC
+db.setUserName("ahlem");//inserer nom de l'utilisateur
+db.setPassword("ahlem");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
