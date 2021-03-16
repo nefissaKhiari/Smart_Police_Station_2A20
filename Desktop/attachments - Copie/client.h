fc06d6df69e4c1d22c7a85045206756fd88a5f03
ahmed407696 <ahmed.ayedi@esprit.tn>
2021-03-16 11:52:09 +0100

    first_commit

---
+++ Desktop/attachments - Copie/client.h
@@ -0,0 +1,30 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QString>
+#include <QSqlQueryModel>
+class Client
+{
+public:
+    Client();
+    Client(int,QString,QString);
+    int getid();
+    QString getnom();
+    QString getprenom();
+    void setid(int);
+    void setnom(QString);
+    void setprenom(QString);
+    bool ajouter();
+    QSqlQueryModel* afficher();
+    bool supprimer(int);
+  //  int rechercher(int);
+private:
+        int id;
+        QString nom, prenom;
+
+
+};
+
+#endif // CLIENT_H
