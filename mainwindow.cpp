5cd28d972c70f31a97b4c7d066eca16da9f811cd
ahlem1202 <zaghouani.ahlem@esprit.tn>
2021-04-01 09:23:19 +0200

    10 eme commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -373,3 +373,58 @@ void MainWindow::on_pb_rechercher_cellule_clicked()
 }
 
 
+
+
+
+void MainWindow::on_imprimer_clicked()
+{
+        QString strStream;
+                QTextStream out(&strStream);
+
+                const int rowCount = ui->tab_detenu->model()->rowCount();
+                const int columnCount = ui->tab_detenu->model()->columnCount();
+                QString TT = QDate::currentDate().toString("yyyy/MM/dd");
+                out <<"<html>\n"
+                      "<head>\n"
+                       "<meta Content=\"Text/html; charset=Windows-1251\">\n"
+                    << "<title>ERP - COMmANDE LIST<title>\n "
+                    << "</head>\n"
+                    "<body bgcolor=#ffffff link=#5000A0>\n"
+                    "<h1 style=\"text-align: center;\"><strong> ****LISTE DES Factures **** "+TT+"</strong></h1>"
+                    "<table style=\"text-align: center; font-size: 20px;\" border=1>\n "
+                      "</br> </br>";
+                // headers
+                out << "<thead><tr bgcolor=#d6e5ff>";
+                for (int column = 0; column < columnCount; column++)
+                    if (!ui->tab_detenu->isColumnHidden(column))
+                        out << QString("<th>%1</th>").arg(ui->tab_detenu->model()->headerData(column, Qt::Horizontal).toString());
+                out << "</tr></thead>\n";
+
+                // data table
+                for (int row = 0; row < rowCount; row++) {
+                    out << "<tr>";
+                    for (int column = 0; column < columnCount; column++) {
+                        if (!ui->tab_detenu->isColumnHidden(column)) {
+                            QString data =ui->tab_detenu->model()->data(ui->tab_detenu->model()->index(row, column)).toString().simplified();
+                            out << QString("<td bkcolor=0>%1</td>").arg((!data.isEmpty()) ? data : QString("&nbsp;"));
+                        }
+                    }
+                    out << "</tr>\n";
+                }
+                out <<  "</table>\n"
+                    "</body>\n"
+                    "</html>\n";
+
+                QTextDocument *document = new QTextDocument();
+                document->setHtml(strStream);
+
+                QPrinter printer;
+
+                QPrintDialog *baba = new QPrintDialog(&printer, NULL);
+                if (baba->exec() == QDialog::Accepted) {
+                    document->print(&printer);
+                }
+
+                delete document;
+
+}
